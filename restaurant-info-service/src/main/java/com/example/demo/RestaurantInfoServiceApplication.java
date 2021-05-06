003f9010130cc493492ec9a58fcc81bd84c60a72
srivatsan <vatsank@gmail.com>
2021-05-06 16:21:58 +0530

    Update

--- restaurant-info-service/src/main/java/com/example/demo/RestaurantInfoServiceApplication.java
+++ restaurant-info-service/src/main/java/com/example/demo/RestaurantInfoServiceApplication.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.example.demo.repos.RestaurantInfoRepository;
+import com.example.demo.entity.*;
 @SpringBootApplication
 public class RestaurantInfoServiceApplication {
 
@@ -10,4 +15,22 @@ public class RestaurantInfoServiceApplication {
 		SpringApplication.run(RestaurantInfoServiceApplication.class, args);
 	}
 
+	
+	@Bean
+	public CommandLineRunner runner() {
+		
+		return new CommandLineRunner() {
+			
+			@Autowired
+			RestaurantInfoRepository repo;
+			
+			@Override
+			public void run(String... args) throws Exception {
+				
+				repo.save(new RestaurantInfo(101, "Gokul Tiffins", "chennai", "gokul@abc.com", "veg", "6.00 am to 10.00 P.m"));
+				
+				
+			}
+		};
+	}
 }
