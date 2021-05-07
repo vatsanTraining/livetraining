62d153b94d617c0177cb2d2cccaace99f2cf89cd
srivatsan <vatsank@gmail.com>
2021-05-07 10:33:00 +0530

    update

--- restaurant-info-service/src/main/java/com/example/demo/repos/RestaurantInfoRepository.java
+++ restaurant-info-service/src/main/java/com/example/demo/repos/RestaurantInfoRepository.java
@@ -1,6 +1,11 @@
 package com.example.demo.repos;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.RestaurantInfo;
@@ -8,4 +13,12 @@ import com.example.demo.entity.RestaurantInfo;
 @Repository
 public interface RestaurantInfoRepository extends JpaRepository<RestaurantInfo, Integer> {
 
+	public RestaurantInfo findByName(String srchName);
+	
+	@Query("update RestaurantInfo set openingHours=:newValue where id=:srchId")
+	@Modifying
+	@Transactional
+	public int updateOpeningHours(@Param("srchId") int id,@Param("newValue") String valueToChange);
+	
+	
 }
