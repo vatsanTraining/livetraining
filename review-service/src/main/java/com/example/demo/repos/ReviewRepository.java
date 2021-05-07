fab2d6d5170dbad4b6735251c7f222175769331d
srivatsan <vatsank@gmail.com>
2021-05-07 12:22:13 +0530

    Update

--- review-service/src/main/java/com/example/demo/repos/ReviewRepository.java
+++ review-service/src/main/java/com/example/demo/repos/ReviewRepository.java
@@ -2,11 +2,16 @@ package com.example.demo.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.demo.entity.Review;
-
+import java.util.*;
 @RepositoryRestResource
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
+
+	@RestResource(path = "names" )
 	public Review findByRestaurantName(String srchName);
+	@RestResource(path = "rating" )
+	public List<Review> findByReviewRatingGreaterThan(double srchValue);
 }
