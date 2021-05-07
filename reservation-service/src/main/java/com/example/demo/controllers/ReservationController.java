2ae518b0674ee2597724593161eb85ccc84a0e36
srivatsan <vatsank@gmail.com>
2021-05-07 14:09:19 +0530

    Update

--- reservation-service/src/main/java/com/example/demo/controllers/ReservationController.java
+++ reservation-service/src/main/java/com/example/demo/controllers/ReservationController.java
@@ -1,5 +1,29 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Reservation;
+import com.example.demo.repo.ReservationRepository;
+import java.util.*;
+@RestController
 public class ReservationController {
 
+	
+	@Autowired
+	private ReservationRepository repo;
+	
+	
+	
+	@GetMapping(path = "/api/v1/reservations")
+	public CollectionModel<Reservation> findAll(){
+		
+		List<Reservation> list = this.repo.findAll();
+		
+		
+		return null;
+		
+	}
 }
