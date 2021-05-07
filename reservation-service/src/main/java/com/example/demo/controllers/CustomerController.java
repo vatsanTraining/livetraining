da5409d4d33da5be06eb173d5c96b45c9bf96471
srivatsan <vatsank@gmail.com>
2021-05-07 14:05:33 +0530

    Update

--- reservation-service/src/main/java/com/example/demo/controllers/CustomerController.java
+++ reservation-service/src/main/java/com/example/demo/controllers/CustomerController.java
@@ -1,5 +1,25 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Customer;
+import com.example.demo.repo.CustomerRepository;
+
+@RestController
 public class CustomerController {
 
+	
+	@Autowired
+	private CustomerRepository repo;
+	
+	
+	@GetMapping(path = "/customers/{id}")
+	public Customer getCustomerById(@PathVariable("id") int id) {
+		
+		return this.repo.findById(id).get();
+		
+	}
 }
