585a4ef4cc1412004848c4c9ab8251b970eb89f6
srivatsan <vatsank@gmail.com>
2021-05-11 14:26:47 +0530

    Update

--- secured-service/src/main/java/com/example/demo/SecurityConfig.java
+++ secured-service/src/main/java/com/example/demo/SecurityConfig.java
@@ -1,23 +1,42 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired
+	PasswordEncoder encoder;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+
+		
+		auth.inMemoryAuthentication()
+		       .withUser("india").password(encoder.encode("india")).roles("ADMIN")
+		        .and().withUser("chennai").password(encoder.encode("chennai")).roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+
+		  http.authorizeRequests().antMatchers("/accounts/**")
+		               .authenticated().and().httpBasic();
+	
 	}
 
 	
