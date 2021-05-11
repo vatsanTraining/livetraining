f0b8f9b77a6105a7937d402a24c4b5e2273f2b19
srivatsan <vatsank@gmail.com>
2021-05-11 15:46:40 +0530

    Update

--- webclient-for-secured-service/src/main/java/com/example/demo/controllers/OrderClientController.java
+++ webclient-for-secured-service/src/main/java/com/example/demo/controllers/OrderClientController.java
@@ -23,7 +23,7 @@ public class OrderClientController {
 	public Mono<String> getOrder() {
 		
 		return client.get().uri("http://localhost:6565/orders")
-				.headers(header -> header.setBasicAuth("nepal","nepal"))
+				.headers(header -> header.setBasicAuth("india","india"))
 				 .retrieve().onStatus(HttpStatus::is5xxServerError, 
 						  clientResponse ->
 			        Mono.error(new RuntimeException("User Not Authenticated")))
