780109908df654c1d3b01c8f845ed35db40122f4
srivatsan <vatsank@gmail.com>
2021-05-11 16:22:44 +0530

    Update

--- secured-service/src/main/java/com/example/demo/JdbcSecurityConfig.java
+++ secured-service/src/main/java/com/example/demo/JdbcSecurityConfig.java
@@ -1,36 +1,39 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableGlobalMethodSecurity(jsr250Enabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class JdbcSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	
-	@Bean
-	public PasswordEncoder encoder() {
-		
-		return new BCryptPasswordEncoder();
-	}
 	
 	@Autowired
-	PasswordEncoder encoder;
+	private DataSource dataSource;
+	
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication()
+        .dataSource(dataSource)
+        .usersByUsernameQuery(
+                "SELECT username, password, enabled from users where username = ?")
+            .authoritiesByUsernameQuery(
+                "SELECT u.username, a.authority " +
+                "FROM authorities a, users u " +
+                "WHERE u.username = ? " +
+                "AND u.username = a.username"
+            )
+        .passwordEncoder(new BCryptPasswordEncoder());
 
-		
-		auth.inMemoryAuthentication()
-		       .withUser("india").password(encoder.encode("india")).roles("ADMIN")
-		        .and().withUser("chennai").password(encoder.encode("chennai")).roles("USER");
+	
 	}
 
 	@Override
@@ -41,7 +44,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	}
 
-	
-	
-	
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+
+	return new BCryptPasswordEncoder();
+	}
+
 }
---
+++ secured-service/src/main/java/org/module/demo/SecurityConfig.java
@@ -0,0 +1,47 @@
+package org.module.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired
+	PasswordEncoder encoder;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		
+		auth.inMemoryAuthentication()
+		       .withUser("india").password(encoder.encode("india")).roles("ADMIN")
+		        .and().withUser("chennai").password(encoder.encode("chennai")).roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		  http.authorizeRequests().antMatchers("/orders/**")
+		               .authenticated().and().httpBasic();
+	
+	}
+
+	
+	
+	
+}
