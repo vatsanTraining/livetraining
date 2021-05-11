d68edca45033ecaadab99d644049c97794026a85
srivatsan <vatsank@gmail.com>
2021-05-11 12:03:32 +0530

    Customer EndPoint Class

---
+++ account-service/src/main/java/com/example/demo/utils/DocumentationEndpoint.java
@@ -0,0 +1,28 @@
+package com.example.demo.utils;
+
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
+import org.springframework.stereotype.Component;
+
+@Component
+@WebEndpoint(id = "docs")
+public class DocumentationEndpoint {
+
+	
+	String versionInfo = new StringBuffer().append("Account Service").append("\n").append("Version 1.0").toString();
+
+	@ReadOperation
+	public String releaseInfo() {
+	   return versionInfo;
+	  }
+
+	
+	@WriteOperation
+	public String updateInfo(@Selector String author) {
+	   return versionInfo.concat(author);
+	  }
+
+	
+}
