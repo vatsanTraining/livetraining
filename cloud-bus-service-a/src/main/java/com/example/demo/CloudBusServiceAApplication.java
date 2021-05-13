4694e5c9f96b9995e5dc6736ddde604285f0066e
srivatsan <vatsank@gmail.com>
2021-05-13 14:41:31 +0530

    Cloud Bus Service -A

---
+++ cloud-bus-service-a/src/main/java/com/example/demo/CloudBusServiceAApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBusServiceAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBusServiceAApplication.class, args);
+	}
+
+}
